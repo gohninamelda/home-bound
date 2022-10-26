3424a2c1a462c9e7afbb8bba678712487755ffe3
meldameldamelda <melda.goh.2021@scis.smu.edu.sg>
2022-10-26 18:47:50 +0800

    firebase config

---
+++ firebase config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXI_8Vo1q54jXzgP3vJqQJE-NNd_4d-Yw",
+    authDomain: "homebound-ed9c8.firebaseapp.com",
+    projectId: "homebound-ed9c8",
+    storageBucket: "homebound-ed9c8.appspot.com",
+    messagingSenderId: "849650603194",
+    appId: "1:849650603194:web:8bf6a707888e53860e9e83"
+  };
